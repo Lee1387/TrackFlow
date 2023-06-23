8500aaafda7182ff3b9c92ec5d63eb1376710e72
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-06-23 04:18:24 +0100

    add project post new member route

--- trackflow-api/routes/projects.js
+++ trackflow-api/routes/projects.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const { Project, generateJoinCode } = require("../models/project");
+const { Project, Member, generateJoinCode } = require("../models/project");
 const router = express.Router();
 
 // Get specified project
@@ -25,6 +25,23 @@ router.post("/", async (req, res) => {
     project = await project.save();
     res.send(project);
 });
+
+// Post a new project member
+router.post("/:id/members", async (req, res) => {
+    let project = await Project.findById(req.params.id);
+    if (!project)
+        return res.status(404).send("The project with the given ID was not found.");
+
+    let newMember = new Member({
+        user: req.body.userId,
+        project: req.params.id,
+    });
+    newMember = await newMember.save();
+
+    project.members.push(newMember);
+    project = await project.save();
+    res.send(project);
+})
 
 // Put specified project
 router.put("/:id", async (req, res) => {
