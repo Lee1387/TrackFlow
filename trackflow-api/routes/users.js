64292490263a9073fb72ab57b02740b270f940cf
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-06-23 03:40:46 +0100

    add user post and put routes

--- trackflow-api/routes/users.js
+++ trackflow-api/routes/users.js
@@ -12,10 +12,33 @@ router.get("/me", async (req, res) => {
 // Get specified user
 router.get("/:id", async (req, res) => {
     const user = await User.findById(req.params.id).select("-password");
-    if (!user) return res.status(404).send("The user with the given ID was not found.")
+    if (!user) return res.status(404).send("The user with the given ID was not found.");
         res.send(user);
 });
 
 // Post new user
+router.post("/", async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    user = await user.save();
+    res.send(user);
+});
+
+// Put specified user
+router.put("./:id", async (req, res) => {
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        { name: req.body.name },
+        { new: true }
+    );
+    if (!user) return res.status(404).send("The user with the given ID was not found.");
+
+    res.send(user);
+});
+
+// Delete specified user
 
 module.exports = router;
