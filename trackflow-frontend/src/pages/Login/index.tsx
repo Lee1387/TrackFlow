2ef6781863e927a2426552a57b80838998737352
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-06-24 02:52:51 +0100

    added theme.tsx and updates to login page

--- trackflow-frontend/src/pages/Login/index.tsx
+++ trackflow-frontend/src/pages/Login/index.tsx
@@ -31,7 +31,19 @@ const Login = () => {
                     <LoginForm />
                     <Typography variant="body1" color="typography.main">
                         {"Don't have an account? "}
-                        <Link to="/register">{"Sign Up"}</Link>
+                        <Typography
+                            component={Link}
+                            to="/register"
+                            sx={{
+                                textDecoration: "none",
+                                color: "secondary.main",
+                                "&:visited": {
+                                    color: "secondary.main",
+                                },
+                            }}
+                        >
+                            Sign Up
+                        </Typography>
                     </Typography>
             </Grid>
             <Grid
