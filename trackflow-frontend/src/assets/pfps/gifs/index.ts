07a42627abcf450b58b78b8dd3671446a7d13cd8
Lee1387 <127062297+Lee1387@users.noreply.github.com>
2023-06-28 04:19:37 +0100

    Add PfpSelector component to registration

---
+++ trackflow-frontend/src/assets/pfps/gifs/index.ts
@@ -0,0 +1,11 @@
+import pfp1 from "./1_gif.gif";
+import pfp2 from './2_gif.gif';
+import pfp3 from './3_gif.gif';
+import pfp4 from './4_gif.gif';
+import pfp5 from './5_gif.gif';
+import pfp6 from './6_gif.gif';
+import pfp7 from './7_gif.gif';
+
+const pfpList = [pfp1, pfp2,  pfp3, pfp4, pfp5, pfp6, pfp7];
+
+export default pfpList;
